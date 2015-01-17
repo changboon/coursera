ceddbdfc956b164e414df9f85f67eec8433b622e
changboon <boon.heng@gmail.com>
2015-01-18 00:55:41 +0800

    RR PA2 update

---
+++ 05. Reproducible Research/Peer Assessment 2/pa2.R
@@ -0,0 +1,9 @@
+setwd("~/GitHub/coursera/05. Reproducible Research/Peer Assessment 2")
+
+stormData <- read.csv(".//repdata-data-StormData.csv")
+
+head(stormData)
+
+#EVTYPE FATALITIES INJURIES 
+
+#EVTYPE PROPDMG PROPDMGEXP CROPDMG CROPDMGEXP
