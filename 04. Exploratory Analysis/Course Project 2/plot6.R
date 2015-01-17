94277513fcb856c856dc96a88f382a62d011b111
changboon <boon.heng@gmail.com>
2015-01-18 00:19:18 +0800

    EDA CP2 misc

--- 04. Exploratory Analysis/Course Project 2/plot6.R
+++ 04. Exploratory Analysis/Course Project 2/plot6.R
@@ -1,6 +1,7 @@
 setwd("~/Github/coursera/04. Exploratory Analysis/Course Project 2")
 
 library(ggplot2)
+library(plyr)
 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
@@ -15,10 +16,15 @@ losangeles <- data[data$fips=="06037",]
 cut_losangeles <- losangeles[losangeles$type=="ON-ROAD",]
 agg_losangeles <- aggregate(Emissions~year, cut_losangeles, sum)
 
+agg_losangeles <- mutate(agg_losangeles, City = "Los Angeles")
+agg_baltimore <- mutate(agg_baltimore, City = "Baltimore")
+
 agg_baltimore$year <- as.Date(paste0(agg_baltimore$year,"-01-01"))
 agg_losangeles$year <- as.Date(paste0(agg_losangeles$year,"-01-01"))
 
+combined <- rbind(agg_baltimore, agg_losangeles)
+
 png("plot6.png", width= 640, height=640)
-qplot(year, Emissions, data=agg_baltimore, geom="line", main="Total PM2.5 Emissions Across Years in Baltimore from Motor"
+qplot(year, Emissions, data=combined, facets=.~City, geom="line", main="Total PM2.5 Emissions Across Years from Motor"
       , xlab="Year", ylab="Emissions (Tons)")
 dev.off()
