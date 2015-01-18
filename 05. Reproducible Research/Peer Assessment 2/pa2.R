c78fd05ee8bc0dc0e01239d4faf7fcb1ac126a4d
changboon <boon.heng@gmail.com>
2015-01-18 20:59:23 +0800

    RR PA2 Update

--- 05. Reproducible Research/Peer Assessment 2/pa2.R
+++ 05. Reproducible Research/Peer Assessment 2/pa2.R
@@ -1,9 +1,16 @@
 setwd("~/GitHub/coursera/05. Reproducible Research/Peer Assessment 2")
 
-stormData <- read.csv(".//repdata-data-StormData.csv")
+library(data.table)
 
-head(stormData)
+stormData <- data.table(read.csv(".//repdata-data-StormData.csv"))
+setkey(stormData, EVTYPE)
+names(stormData)
 
 #EVTYPE FATALITIES INJURIES 
+pophealth <- stormData[, list(FATALITIES=sum(FATALITIES), INJURIES=sum(INJURIES)), by=list(EVTYPE)]
+head(pophealth)
 
 #EVTYPE PROPDMG PROPDMGEXP CROPDMG CROPDMGEXP
+econ <- stormData[, list(PROPDMG=sum(PROPDMG), PROPDMGEXP=sum(PROPDMGEXP), CROPDMG=sum(CROPDMG), CROPDMGEXP=sum(CROPDMGEXP))
+                  , by=list(EVTYPE)]
+head(econ)
