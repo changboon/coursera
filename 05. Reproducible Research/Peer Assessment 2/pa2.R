80642f3c9b655980796d53d2425d07cf22ada30e
changboon <boon.heng@gmail.com>
2015-01-19 17:30:07 +0800

    RR PA2 Update

--- 05. Reproducible Research/Peer Assessment 2/pa2.R
+++ 05. Reproducible Research/Peer Assessment 2/pa2.R
@@ -3,17 +3,31 @@ setwd("~/GitHub/coursera/05. Reproducible Research/Peer Assessment 2")
 library(data.table)
 library(reshape)
 library(ggplot2)
+library(plyr)
 
 stormData <- data.table(read.csv(".//repdata-data-StormData.csv"))
-setkey(stormData, EVTYPE)
-names(stormData)
 
 #EVTYPE FATALITIES INJURIES 
 pophealth <- stormData[, list(FATALITIES=sum(FATALITIES), INJURIES=sum(INJURIES)), by=list(EVTYPE)]
-pophealth <- melt(pophealth, id="EVTYPE")
+pophealth <- mutate(pophealth, TotalHarm = FATALITIES + INJURIES)
+totalHarm <- subset(pophealth[order(-rank(TotalHarm))], , select=c(EVTYPE, TotalHarm))[1:10] 
+totalHarm <- melt(totalHarm, id="EVTYPE")
+fatalities <- subset(pophealth[order(-rank(FATALITIES))], , select=c(EVTYPE, FATALITIES))[1:10] 
+fatalities <- melt(fatalities, id="EVTYPE")
+injuries <- subset(pophealth[order(-rank(INJURIES))], , select=c(EVTYPE, INJURIES))[1:10] 
+injuries <- melt(injuries, id="EVTYPE")
+pophealth <- rbind(totalHarm, fatalities, injuries)
 
-png("pophealth.png", width= 640, height=640)
-qplot(EVTYPE, value, data=pophealth, facets=variable~.)
+tornado <- pophealth[pophealth$EVTYPE == "TORNADO",]
+pophealth <- pophealth[pophealth$EVTYPE != "TORNADO",] #remove tornado because most are from tornado
+
+png("pophealth.png", width= 800, height=640)
+ggplot(data=pophealth, aes(x=reorder(EVTYPE, -value), y=value)) + 
+  geom_bar(stat="identity") +
+  facet_grid(variable ~ .) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  xlab("Event Type") +
+  ylab("Number affected (Pax)")
 dev.off()
 
 head(pophealth)
